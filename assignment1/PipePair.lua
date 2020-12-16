ce2412d8f91c11616fb0383be7cfc0ef95118518
JadeRyahn <72679288+JadeRyahn@users.noreply.github.com>
2020-12-16 21:21:28 +0800

    Add files via upload

--- assignment1/PipePair.lua
+++ assignment1/PipePair.lua
@@ -11,7 +11,7 @@
 PipePair = Class{}
 
 -- size of the gap between pipes
-local GAP_HEIGHT = 90
+local GAP_HEIGHT = 75 + math.random(-30, 30)
 
 function PipePair:init(y)
     -- flag to hold whether this pair has been scored (jumped through)
